a34a6c2a01604ac5704f704aee89e2f26ff734be
Eric van Riet Paap <ericvrp@gmail.com>
2019-02-04 13:46:45 +0100

    Support regular keyboard in addition to the usb/midi interface

--- boomklat.js
+++ boomklat.js
@@ -3,67 +3,170 @@
 const exec = require('child_process').exec
 const midi = require('midi')
 
-// Set up a new input.
-const input = new midi.input();
 
-const nPorts = input.getPortCount();
-// console.log('nPorts:', nPorts);
-for (var portNumber = 0;portNumber < nPorts;portNumber++) {
-  if (input.getPortName(portNumber).includes('UM-ONE'))
-    break
-}
+// settings
+const PORT_NAME   = 'UM-ONE'
 
-// console.log('portNumber:', portNumber);
+const NOTE_ON     = 153
 
-// const portName = input.getPortName(portNumber);
-// console.log('portName:', portName);
+const VOLUME_THRESHOLD = 25
 
-// Configure a callback.
-input.on('message', function(deltaTime, message) {
+const DRUM_FOOT   = 36
+const DRUM_RED    = 38
+const DRUM_BLUE   = 48
+const DRUM_GREEN  = 45
+const DRUM_YELLOW = 46
+const DRUM_ORANGE = 49
+
+
+//
+const playNote = (message) => {
+  if (!message) {
+    // console.info('warning: no message')
+    return
+  }
+
   // The message is an array of numbers corresponding to the MIDI bytes:
   //   [status, data1, data2]
   // https://www.cs.cf.ac.uk/Dave/Multimedia/node158.html has some helpful
   // information interpreting the messages.
-  const drumInfo = {
-    36: {color:'foot'  , volumeOffset:-25, volumeMultiplier:5.0, sample:'foot.wav'  },
-    38: {color:'red'   , volumeOffset:-25, volumeMultiplier:5.0, sample:'red.wav'   },
-    48: {color:'blue'  , volumeOffset:-25, volumeMultiplier:5.0, sample:'blue.wav'  },
-    45: {color:'green' , volumeOffset:-25, volumeMultiplier:5.0, sample:'green.wav' },
-    46: {color:'yellow', volumeOffset:-25, volumeMultiplier:5.0, sample:'yellow.wav'},
-    49: {color:'orange', volumeOffset:-25, volumeMultiplier:5.0, sample:'orange.wav'},
-  }
+  const drumInfo = {}
+  drumInfo[DRUM_FOOT  ] = {color:'foot'  , volumeOffset:-VOLUME_THRESHOLD, volumeMultiplier:5.0, sample:'foot.wav'  }
+  drumInfo[DRUM_RED   ] = {color:'red'   , volumeOffset:-VOLUME_THRESHOLD, volumeMultiplier:5.0, sample:'red.wav'   }
+  drumInfo[DRUM_BLUE  ] = {color:'blue'  , volumeOffset:-VOLUME_THRESHOLD, volumeMultiplier:5.0, sample:'blue.wav'  }
+  drumInfo[DRUM_GREEN ] = {color:'green' , volumeOffset:-VOLUME_THRESHOLD, volumeMultiplier:5.0, sample:'green.wav' }
+  drumInfo[DRUM_YELLOW] = {color:'yellow', volumeOffset:-VOLUME_THRESHOLD, volumeMultiplier:5.0, sample:'yellow.wav'}
+  drumInfo[DRUM_ORANGE] = {color:'orange', volumeOffset:-VOLUME_THRESHOLD, volumeMultiplier:5.0, sample:'orange.wav'}
 
-  const [status, drumNumber, force] = message;
+  const [status, drumNumber, force] = message
   const drum = drumInfo[drumNumber]
-  if (status == 153 && drum) {
-    let volume = (force + drum.volumeOffset) * drum.volumeMultiplier / 256.0;
+  // console.log(drum, drumNumber, drumInfo)
+
+  if (status === NOTE_ON && drum) {
+    let volume = (force + drum.volumeOffset) * drum.volumeMultiplier / 256.0
     volume = Math.max(volume, 0.1)
     volume = Math.min(volume, 5.0)
-    console.log(drum.color, volume.toFixed(1), drum.sample);
+    console.log(drum.color, volume.toFixed(1), drum.sample)
     const cmd = `play -v ${volume.toFixed(1)} samples/${drum.sample}`
-    // console.log(cmd);
-    exec(cmd);
+    // console.log(cmd)
+    exec(cmd)
   } else {
-     console.log('message:', message, ', deltaTime:', deltaTime);
+    console.info('warning: unknown message', message)
   }
-});
+} // end of playNote()
 
-// Open the first available input port
-var openPort = input.openPort(portNumber);
-// console.log('openPort:', openPort);
 
-// Sysex, timing, and active sensing messages are ignored
-// by default. To enable these message types, pass false for
-// the appropriate type in the function below.
-// Order: (Sysex, Timing, Active Sensing)
-// For example if you want to receive only MIDI Clock beats
-// you should use
-// input.ignoreTypes(true, false, true)
-var ignoreTypes = input.ignoreTypes(false, false, false);
-// console.log('ignoreTypes', ignoreTypes);
+//
+const handleKeyboard = () => {
+  var stdin = process.stdin
 
-// ... receive MIDI messages ...
+  // without this, we would only get streams once enter is pressed
+  stdin.setRawMode( true )
 
-// Close the port when done.
-// var closePort = input.closePort();
-// console.log('closePort', closePort);
+  // resume stdin in the parent process (node app won't quit all by itself
+  // unless an error or process.exit() happens)
+  stdin.resume()
+
+  // i don't want binary, do you?
+  stdin.setEncoding( 'utf8' )
+
+  // on any data into stdin
+  stdin.on( 'data', function( key ){
+    // ctrl-c ( end of text )
+    if ( key === '\u0003' ) {
+      process.exit()
+    }
+    // write the key to stdout all normal like
+    // process.stdout.write( key )
+    const key2Message = {
+      ' ': [NOTE_ON, DRUM_FOOT  , VOLUME_THRESHOLD *  3],
+
+      'z': [NOTE_ON, DRUM_FOOT  , VOLUME_THRESHOLD *  1],
+      'x': [NOTE_ON, DRUM_RED   , VOLUME_THRESHOLD *  1],
+      'c': [NOTE_ON, DRUM_BLUE  , VOLUME_THRESHOLD *  1],
+      'v': [NOTE_ON, DRUM_GREEN , VOLUME_THRESHOLD *  1],
+      'b': [NOTE_ON, DRUM_YELLOW, VOLUME_THRESHOLD *  1],
+      'n': [NOTE_ON, DRUM_ORANGE, VOLUME_THRESHOLD *  1],
+
+      'a': [NOTE_ON, DRUM_FOOT  , VOLUME_THRESHOLD *  3],
+      's': [NOTE_ON, DRUM_RED   , VOLUME_THRESHOLD *  3],
+      'd': [NOTE_ON, DRUM_BLUE  , VOLUME_THRESHOLD *  3],
+      'f': [NOTE_ON, DRUM_GREEN , VOLUME_THRESHOLD *  3],
+      'g': [NOTE_ON, DRUM_YELLOW, VOLUME_THRESHOLD *  3],
+      'h': [NOTE_ON, DRUM_ORANGE, VOLUME_THRESHOLD *  3],
+
+      'q': [NOTE_ON, DRUM_FOOT  , VOLUME_THRESHOLD *  6],
+      'w': [NOTE_ON, DRUM_RED   , VOLUME_THRESHOLD *  6],
+      'e': [NOTE_ON, DRUM_BLUE  , VOLUME_THRESHOLD *  6],
+      'r': [NOTE_ON, DRUM_GREEN , VOLUME_THRESHOLD *  6],
+      't': [NOTE_ON, DRUM_YELLOW, VOLUME_THRESHOLD *  6],
+      'y': [NOTE_ON, DRUM_ORANGE, VOLUME_THRESHOLD *  6],
+
+      '1': [NOTE_ON, DRUM_FOOT  , VOLUME_THRESHOLD *  9],
+      '2': [NOTE_ON, DRUM_RED   , VOLUME_THRESHOLD *  9],
+      '3': [NOTE_ON, DRUM_BLUE  , VOLUME_THRESHOLD *  9],
+      '4': [NOTE_ON, DRUM_GREEN , VOLUME_THRESHOLD *  9],
+      '5': [NOTE_ON, DRUM_YELLOW, VOLUME_THRESHOLD *  9],
+      '6': [NOTE_ON, DRUM_ORANGE, VOLUME_THRESHOLD *  9],
+    }
+
+    // console.log( key2Message[key] )
+    playNote(key2Message[key])
+  })
+} // end of handleKeyboard()
+
+
+//
+const handleUsb = () => {
+  // Set up a new input.
+  const input = new midi.input()
+
+  const nPorts = input.getPortCount()
+  // console.log('nPorts:', nPorts)
+  for (var portNumber = 0;portNumber < nPorts;portNumber++) {
+    if (input.getPortName(portNumber).includes(PORT_NAME))
+      break
+  }
+  if (portNumber >= nPorts) {
+    console.info('warning:', PORT_NAME, 'usb port not found')
+    return
+  }
+
+  // console.log('portNumber:', portNumber)
+
+  // const portName = input.getPortName(portNumber)
+  // console.log('portName:', portName)
+
+  // Configure a callback.
+  input.on('message', function(deltaTime, message) {
+    playNote(message)
+  })
+
+  // Open the first available input port
+  var openPort = input.openPort(portNumber)
+  // console.log('openPort:', openPort)
+
+  // Sysex, timing, and active sensing messages are ignored
+  // by default. To enable these message types, pass false for
+  // the appropriate type in the function below.
+  // Order: (Sysex, Timing, Active Sensing)
+  // For example if you want to receive only MIDI Clock beats
+  // you should use
+  // input.ignoreTypes(true, false, true)
+  var ignoreTypes = input.ignoreTypes(false, false, false)
+  // console.log('ignoreTypes', ignoreTypes)
+
+  // ... receive MIDI messages ...
+
+  // Close the port when done.
+  // var closePort = input.closePort()
+  // console.log('closePort', closePort)
+} // end of handleUsb()
+
+
+//
+handleUsb()
+handleKeyboard()
+
+
+// the end
